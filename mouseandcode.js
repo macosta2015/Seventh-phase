be1c57d9d85cf87328cd58a2996923c5b608f793
macosta2015 <macosta2015@my.fit.edu>
2024-03-24 23:08:41 -0400

    commit

--- mouseandcode.js
+++ mouseandcode.js
@@ -1,80 +1,15 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
+const { launchBrowserAndNavigateToDocument } = require('./components/launchBrowserAndNavigate.js');
+
+
+
 (async () => {
     try {
-        //LAUNCHING BROWSER
-        console.log('//LAUNCHING BROWSER');
-        const browser = await puppeteer.launch({
-            defaultViewport: { width: 800, height: 800 },
-            headless: false,
-            userDataDir: '/Users/marioacosta/Library/Application Support/Google/Chrome/Profile 1'
-        });
-        console.log('Browser launched successfully.');
+        const newPage = await launchBrowserAndNavigateToDocument(); // This line should return a newPage object
 
-        const newPage = await browser.newPage();
-        console.log('New page opened successfully.');
 
-        await newPage.goto('https://cad.onshape.com/documents?resourceType=resourcecompanyowner&nodeId=65efc5e06e5bec02f57742fe', { waitUntil: 'networkidle0', timeout: 0 });
-        console.log('Page loaded successfully.');
-
-        await newPage.type('input[name="email"].form-control', process.env.EMAIL);
-        console.log('Typed email successfully.');
-
-        await newPage.type('input[name="password"].form-control', process.env.PASSWORD);
-        console.log('Typed password successfully.');
-
-        console.log(await newPage.$eval('input[name="email"].form-control', input => input.getBoundingClientRect()));
-        console.log(await newPage.$eval('input[name="password"].form-control', input => input.getBoundingClientRect()));
-
-        await newPage.click('button.btn.btn-primary.os-signin-button');
-        console.log('Clicked on the sign-in button successfully.');
-
-        console.log(await newPage.$eval('button.btn.btn-primary.os-signin-button', button => button.getBoundingClientRect()));
-
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        console.log('Waited for 5 seconds.');
-
-        await newPage.evaluate(() => {
-            const thirdButton = document.querySelectorAll('.documents-filter-icon')[2];
-            if (thirdButton) {
-                thirdButton.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
-                console.log(thirdButton.getBoundingClientRect());
-            } else {
-                console.error('Third button not found.');
-            }
-        });
-
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log('Waited for 2 seconds.');
-
-        await newPage.evaluate(() => {
-            const thirdButton = document.querySelectorAll('.documents-filter-icon')[2];
-            if (thirdButton) {
-                thirdButton.click();
-            } else {
-                console.error('Third button not found.');
-            }
-        });
-
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log('Clicked on the third button.');
-
-        //LAUNCHIG FILE NAME
-        console.log('//LAUNCHIG FILE NAME, Scale Sketch Example - Copy - Copy');
-        await newPage.evaluate(() => {
-            const documentNameElement = document.querySelector('span[aria-label="Document name: Scale Sketch Example - Copy - Copy"][ng-bind-html="document.resultHighlight"]');
-            if (documentNameElement) {
-                documentNameElement.click();
-            } else {
-                console.error('Element with text "Scale Sketch Example - Copy - Copy" not found.');
-            }
-        });
-
-        await new Promise(resolve => setTimeout(resolve, 3000));
-        console.log('Waited for 3 seconds.');
-
-        // ADDING A NEW SKETCH 
         console.log('Waiting for the Sketch button to appear...');
         await newPage.waitForSelector('div.toolset [command-id="newSketch"]', { visible: true });
         console.log('Sketch button found. Clicking on it...');
@@ -86,11 +21,6 @@ require('dotenv').config();
         console.log('Waiting 10 seconds.');
         await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
         await newPage.click('div.ns-dialog-button-ok.button-ok');
-
-
-
-
-
 
 
         // COPY SKETCH AND PASTING IT INTO ANOTHER SKETCH
@@ -171,52 +101,6 @@ require('dotenv').config();
 
 
 
-
-
-
-
-        // //TEST
-        // async function performRightClickOption(newPage) {
-        //     console.log('Right-clicked started.');
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Waited for 5 seconds.');
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Waited for 5 seconds.');
-        //     await newPage.click('div[data-id="Dg4JdGx6jlZTm4XD"]', { button: 'right' });
-        //     console.log('WHERE DID WE CLICK???')
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Right-clicked successfully.');
-
-        //     await newPage.evaluate(() => {
-        //         window.scrollTo(0, document.body.scrollHeight);
-        //     });
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Scrolled to bottom of the page.');
-
-        //     await newPage.waitForSelector('.context-menu-item-span', { visible: true });
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Waiting for context menu items to appear.');
-
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Waited for 5 seconds.');
-
-        //     const editOptions1 = await newPage.evaluate(() => {
-        //         const menuItems = document.querySelectorAll('.context-menu-item-span');
-        //         return Array.from(menuItems).map(item => item.textContent.trim());
-        //     });
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Retrieved edit options.');
-
-        //     console.log('Right-clicked ended.');
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        //     console.log('Waited for 5 seconds.');
-
-        //     return editOptions1;
-        // }
-
-        // // Example usage:
-        // const editOptions1 = await performRightClickOption(newPage);
-        // console.log(editOptions1);
 
 
 
@@ -349,69 +233,10 @@ require('dotenv').config();
 
 
 
-
-
-
-        // await newPage.mouse.move(400, 448);
-        // console.log('Mouse moved to X:400, Y:448');
-        // await newPage.mouse.down({ button: 'right' });
-        // console.log('Right mouse button clicked and held at X:400, Y:448');
-        // await newPage.mouse.up({ button: 'right' }); // Release the right mouse button
-        // console.log('Right mouse button released.');
-
         console.log('Waiting 10 seconds.');
         await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
 
 
-
-        // //RIGHT CLICK OPTIONS  
-        // console.log('RIGHT CLICK OPTIONS:');
-        // await newPage.evaluate(() => {
-        //     const thirdButton = document.querySelectorAll('.os-list-item-name')[2];
-        //     if (thirdButton) {
-        //         thirdButton.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
-        //     } else {
-        //         console.error('Third button not found.');
-        //     }
-        // });
-
-        // await new Promise(resolve => setTimeout(resolve, 2000));
-        // console.log('Waited for 2 seconds.');
-
-        // await newPage.evaluate(() => {
-        //     const fifthButton = document.querySelectorAll('.os-list-item-name')[8]; //What is 5 
-        //     if (fifthButton) {
-        //         fifthButton.click();
-        //     } else {
-        //         console.error('Fifth button not found.');
-        //     }
-        // });
-
-        // await newPage.click('div[data-id="Dg4JdGx6jlZTm4XD"]', { button: 'right' });
-        // console.log('Right-clicked successfully.');
-
-        // await new Promise(resolve => setTimeout(resolve, 10000));
-        // console.log('Waited for 10 seconds.');
-
-        // await newPage.evaluate(() => {
-        //     window.scrollTo(0, document.body.scrollHeight);
-        // });
-
-        // await newPage.waitForSelector('.context-menu-item-span', { visible: true });
-
-        // await new Promise(resolve => setTimeout(resolve, 5000));
-        // console.log('Waited for 5 seconds.');
-
-        // const editOptions = await newPage.evaluate(() => {
-        //     const menuItems = document.querySelectorAll('.context-menu-item-span');
-        //     return Array.from(menuItems).map(item => item.textContent.trim());
-        // });
-
-        // //CLICK ON THE EDIT
-        // console.log('CLICK ON THE EDIT:', editOptions);
-
-        // console.log('Setting desired rename option...');
-        // await new Promise(resolve => setTimeout(resolve, 5000));
 
 
 
